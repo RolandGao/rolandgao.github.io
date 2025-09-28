47158113885d771ab782d6026434f886a16f79f2
Roland Gao <gaoroland@gmail.com>
2025-09-27 21:59:27 -0700

    g

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -3,22 +3,30 @@ const path = require('path');
 
 const domain = 'https://rolandgao.github.io';
 const buildPath = path.join(__dirname, '../build');
+const blogDirectory = path.join(__dirname, '../public/data/blogs');
 
-const pages = [
-  '',
-  '/blogs',
-  '/blog/path_to_agi',
-  '/blog/unsaturated_evals_before_gpt5',
-  // Add paths to your blog posts here
-];
+const blogPages = fs.existsSync(blogDirectory)
+  ? fs
+      .readdirSync(blogDirectory)
+      .filter(fileName => path.extname(fileName) === '.md')
+      .map(fileName => `/blog/${path.basename(fileName, '.md')}`)
+  : [];
+
+const pages = Array.from(
+  new Set([
+    '/',
+    '/blog',
+    ...blogPages,
+  ])
+);
 
 const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemap.org/schemas/sitemap/0.9">
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 ${pages
   .map(
     page => `
   <url>
-    <loc>${domain}${page}</loc>
+    <loc>${domain}${page === '/' ? '/' : page}</loc>
     <changefreq>weekly</changefreq>
     <priority>0.8</priority>
   </url>`
